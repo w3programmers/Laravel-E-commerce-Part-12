0238f48dd130e6c24dce0489acdd297013899b64
Masud Alam <masud.eden@gmail.com>
2019-10-04 15:46:09 +0600

    Laravel-Ecommerce-Part-7

--- routes/admin.php
+++ routes/admin.php
@@ -1,5 +1,4 @@
 <?php
-
 Route::group(['prefix'  =>  'admin'], function () {
 
     Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
@@ -40,6 +39,17 @@ Route::group(['prefix'  =>  'admin'], function () {
             Route::post('/update-values', 'Admin\AttributeValueController@updateValues');
             Route::post('/delete-values', 'Admin\AttributeValueController@deleteValues');
         });
+
+        Route::group(['prefix'  =>   'brands'], function() {
+
+    Route::get('/', 'Admin\BrandController@index')->name('admin.brands.index');
+    Route::get('/create', 'Admin\BrandController@create')->name('admin.brands.create');
+    Route::post('/store', 'Admin\BrandController@store')->name('admin.brands.store');
+    Route::get('/{id}/edit', 'Admin\BrandController@edit')->name('admin.brands.edit');
+    Route::post('/update', 'Admin\BrandController@update')->name('admin.brands.update');
+    Route::get('/{id}/delete', 'Admin\BrandController@delete')->name('admin.brands.delete');
+
+});
 
     });
 });
